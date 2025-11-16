6431a8b4a2676cb02f185dd571d4d59f38b22976
snagy11224-cmyk <snagy11224@gmail.com>
2025-11-17 01:06:44 +0200

    added favorite fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import MovieCard from './components/MovieCard'
-import './css/App.css'
-import Home from './pages/Home'
-import { Routes,Route } from 'react-router-dom'
-import Favorites from './pages/Favorites'
-import NavBar from './components/NavBar'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <div>
-      <NavBar/>
-   <main className="main-content">
-      <Routes> 
-        <Route path="/" element={<Home/>} />
-        <Route path="/favorites" element={<Favorites/>} />
-
-      </Routes>
-   </main>
-</div>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,22 @@
-import MovieCard from "../components/MovieCard"
-import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
 import { searchMovies, getPopularMovies } from "../services/api";
-import "../css/Home.css"
+import "../css/Home.css";
 
-function Home(){
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const [searchQuery , setSearchQuery]= useState("");
-const [movies, setMovies]= useState([]);
-const [error, setError]=useState(null) ;
-const [loading , setLoading]=useState(true)
-
- useEffect(() => {
+  useEffect(() => {
     const loadPopularMovies = async () => {
       try {
-        const popularMovies = await getPopularMovies()
+        const popularMovies = await getPopularMovies();
         setMovies(popularMovies);
       } catch (err) {
         console.log(err);
-        setError("Failed to load movies...")
+        setError("Failed to load movies...");
       } finally {
         setLoading(false);
       }
@@ -26,48 +25,52 @@ const [loading , setLoading]=useState(true)
     loadPopularMovies();
   }, []);
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return
+    if (loading) return
 
-const handleSearch = async (e) => {
-e.preventDefault()
-if(!searchQuery.trim()) return
-if(loading) return
-setLoading(true)
-try{
-const searchResults= await searchMovies(searchQuery)
-setMovies(searchResults)
-setError(null)
-}catch(err){
- console.log(err)
-setError("Failed to search movies...")
-} 
-finally{
-    setLoading(false)
+    setLoading(true)
+    try {
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    } catch (err) {
+        console.log(err)
+        setError("Failed to search movies...")
+    } finally {
+        setLoading(false)
+    }
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+        {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
-};
 
-return <div className="home">
-
-<form onSubmit={handleSearch} className="search-form"> 
-    <input type="text" placeholder="Search for movies" className="search-input" value={searchQuery}
-    onChange={(e)=>setSearchQuery(e.target.value)}
-    />
-    <button type="submit" className="search-button">search</button>
-</form>
-
-{error && <div className="error-message">{error}</div>}
-
-{loading ? <div className="loading">Loading... </div>  : 
-<div className="movies-grid"> 
-{movies.map((movie) => 
-    (
-<MovieCard movie={movie} key={movie.id} />
-))}
-</div>
-
-}
-
-</div>
-
-}
-
-export default Home 
+export default Home;
